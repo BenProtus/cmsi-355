79ef38d40c3977db047faec02bd4e488c15cc732
Jmyerzzz <jacksontmyers@gmail.com>
2019-02-27 12:40:20 -0800

    chat server is working!

--- python/chat_server.py
+++ python/chat_server.py
@@ -11,6 +11,7 @@ class ChatHandler(socketserver.StreamRequestHandler):
     def handle(self):
         client = f'{self.client_address} on {threading.currentThread().getName()}'
         print(f'Connected: {client}')
+
         while True:
             self.wfile.write(f'Submit Username: '.encode('utf-8'))
             name = self.rfile.readline()
@@ -21,14 +22,20 @@ class ChatHandler(socketserver.StreamRequestHandler):
                 for address in names.values():
                     address.wfile.write(f'{name} has joined the chat.\n'.encode('utf-8'))
                 break
+
         while True:
             text = self.rfile.readline().decode('utf-8').rstrip()
+            if text.lower().startswith('/quit'):
+                for address in names.values():
+                    address.wfile.write(f'{name} has left\n'.encode('utf-8'))
+                del names[name]
+                break
+            while not text:
+                self.wfile.write("~No empty messages~\n".encode('utf-8'))
+                text = self.rfile.readline().decode('utf-8').rstrip()
             response = f'{name}: {text}\n'
             for address in names.values():
-                address.wfile.write(response.encode('utf-8'))
-        
-
-        
+                address.wfile.write(response.encode('utf-8'))  
 
 
 with ThreadedTCPServer(('', 43210), ChatHandler) as server:
