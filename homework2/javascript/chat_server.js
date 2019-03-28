a01105fcc7795983e6759c92c7046358e5c67264
Jmyerzzz <jacksontmyers@gmail.com>
2019-03-28 16:21:35 -0700

    working on chat

--- homework2/javascript/chat_server.js
+++ homework2/javascript/chat_server.js
@@ -1,17 +1,29 @@
-const webSocketServer = require('websocket').server;
-const http = require('http');
-const webSocketServerPort = 59001;
+const WebSocket = require('ws');
 
-var history = [];
-var users = [];
+const server = new WebSocket.Server({
+    port: 53211
+})
 
-const server = http.createServer((request, response) => {
-  // N/A, writing WebSocket server
-});
-server.listen(webSocketServerPort, () => {
-  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
-});
+clients = []
 
-const wsServer = new webSocketServer({
-  httpServer: server
-});
+server.on('request', function (request) {
+    console.log('New Connection: ', request.connection.remoteAddress)
+    var connection = request.accept();
+    var userName = false;
+
+    connection.on('message', function (message) {
+        if (message.type === 'utf-8') {
+            if (userName === false) {
+                userName = message.utf8data;
+                clients.push(connection);
+                server.send(userName + ' has joined');
+            } else {
+                clients.forEach(client.send(message.utf8data));
+            }
+        }
+    });
+
+    connection.on('close', function(connection) {
+        list.splice( list.indexOf('connection'), 1 );
+    });
+})
